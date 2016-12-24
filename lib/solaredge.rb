f83b0b8c471ce243c721ee2433f4d9618f490075
Charles Hooper <chooper@plumata.com>
2016-12-24 12:40:39 -0800

    Fetch energy production from API

--- lib/solaredge.rb
+++ lib/solaredge.rb
@@ -1,5 +1,45 @@
+require "json"
+require "excon"
 require "solaredge/version"
 
 module Solaredge
-  # Your code goes here...
+  class Api
+
+    BASE_URL = 'https://monitoringapi.solaredge.com'
+    ALLOWED_TIME_UNITS = %w{QUARTER_OF_AN_HOUR HOUR DAY WEEK MONTH YEAR}
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    # accept Time args
+    def site_energy(site_id:, start_date:, end_date:, time_unit:)
+      start_date = start_date.strftime("%Y-%m-%d")
+      end_date = end_date.strftime("%Y-%m-%d")
+      raise ArgumentError.new("Invalid time_unit #{time_unit.inspect}") if !ALLOWED_TIME_UNITS.include?(time_unit)
+
+      _request('energy', {
+        siteId: site_id,
+        startDate: start_date,
+        endDate: end_date,
+        timeUnit: time_unit
+      })
+    end
+
+    def _request(method, params={})
+      url = if params[:siteId]
+        [BASE_URL, 'site', params.delete(:siteId), "#{method}.json"].join('/')
+      else
+        [BASE_URL, "#{method}.json"].join('/')
+      end
+
+      params[:api_key] = @api_key
+      # build request arguments
+      url += '?' + params.map {|k,v| "#{k}=#{v}"}.join('&')
+
+      response = Excon.get(url)
+      body = response.body
+      JSON.parse(body)
+    end
+  end
 end
--- solaredge.gemspec
+++ solaredge.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Charles Hooper"]
   spec.email         = ["chooper@plumata.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Fetch data from SolarEdge monitoring}
+  spec.description   = %q{Fetch data from SolarEdge monitoring}
+  spec.homepage      = "http://www.charleshooper.com"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -27,6 +27,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "excon", "~> 0.54"
+  spec.add_dependency "pry", "~> 0.10"
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
