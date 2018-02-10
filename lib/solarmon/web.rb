6aac02fdc23b5f018d5d7a251878ba23dacfabed
Charles Hooper <chooper@plumata.com>
2018-02-10 05:47:24 -0800

    Add a health check

--- lib/solarmon/web.rb
+++ lib/solarmon/web.rb
@@ -15,6 +15,24 @@ module SolarMon::Web
       "SolarMon #{SolarMon::VERSION}"
     end
 
+    get '/health' do
+      content_type :json
+      date = Time.now.getutc.to_date
+      date_range = (date - 1)..(date + 1)
+
+      db = SolarMon::Storage.connect_database
+      values = SolarMon::Storage.get_energy_values(db, {date: date_range})
+
+      # find the most recent data point and ensure it's < 12 hours old
+      value = values.sort_by {|v| v[:date]}.last
+      if value[:date] < Time.now - 60*60*12
+        halt 500, {status: 'FAIL', msg: 'data is not fresh', most_recent_value: value}.to_json
+      end
+
+      SolarMon::Storage.disconnect_database(db)
+      {status: 'OK', most_recent_value: value}.to_json
+    end
+
     get '/energy.json' do
       content_type :json
       date = Time.now.getutc.to_date
